eda8b5f238cd95bd918b6fb722edaa62db4e732e
mcvax <chris@mcleanfamily.net>
2015-02-06 09:48:38 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -21,3 +21,36 @@
 # on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
 # is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 # has at least a table called data.
+
+
+#
+
+import scraperwiki
+import lxml.html
+import time
+html = scraperwiki.scrape("http://www.readings.com.au/collection/bestselling-books")
+root = lxml.html.fromstring(html)
+
+pos = 0
+for el in root.cssselect("div.text year-end-review a"):
+title = el.cssselect("h1")[0].text_content()
+artist = el.cssselect("H2")[0].text_content()
+publisher = el.cssselect("H3")[0].text_content()
+link = el.attrib['href']
+isbn = link.split("/")[2]
+pos += 1
+
+#print title
+#print author
+#print link
+print isbn
+link = "http://www.readings.com.au" + link
+record = {"title" : title,
+"artist" : artist,
+"publisher" : publisher,
+"isbn" : isbn,
+"link" : link,
+"pos" : pos,
+"sdate" : time.strftime( "%Y-%m-%d" )
+}
+scraperwiki.sqlite.save(unique_keys=["isbn", "sdate"], data=record)
