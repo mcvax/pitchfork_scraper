bc057ddc06ac45f438e761cf7752d6fdbabc8f0b
mcvax <chris@mcleanfamily.net>
2015-02-09 09:47:54 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,44 +22,63 @@
 # is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 # has at least a table called data.
 
-
-#
-
 import scraperwiki
 import lxml.html
 import time
-#html = scraperwiki.scrape("http://pitchfork.com/features/staff-lists/9465-the-top-100-albums-of-2010-2014/")
-#root = lxml.html.fromstring(html)
 
-html = scraperwiki.scrape("http://pitchfork.com/features/staff-lists/9465-the-top-100-albums-of-2010-2014")
-root = lxml.html.fromstring(html)
+basehtml = "http://pitchfork.com/features/staff-lists/9465-the-top-100-albums-of-2010-2014"
+page = 1
 
-for el in root.cssselect("div[id^='album']"):
+while page < 3:
+
+  html = scrapePage(basehtml + )
+
+  root = lxml.html.fromstring(html)
+
+  for el in root.cssselect("div[id^='album']"):
   
-    #mytest = el.text_content()
-    #print str(mytest.encode('utf-8'))
+      #mytest = el.text_content()
+      #print str(mytest.encode('utf-8'))
+      
+      if len(el.cssselect('div.inner div.title h1')) ==0 : continue
+      title = el.cssselect('div.inner div.title h2')[0].text_content()
+      artist = el.cssselect("div.inner div.title h1")[0].text_content()
+      publisher = el.cssselect("div.inner div.title h3")[0].text_content()  
+      rank = el.cssselect("div.inner div.review-content div.rank")[0].text_content()
+      reviewtext = el.cssselect("div.inner div.review-content p")[0].text_content()  
+      #link = el.attrib['href']
+      #isbn = link.split("/")[2]
     
-    if len(el.cssselect('div.inner div.title h1')) ==0 : continue
-    title = el.cssselect('div.inner div.title h2')[0].text_content()
-    artist = el.cssselect("div.inner div.title h1")[0].text_content()
-    publisher = el.cssselect("div.inner div.title h3")[0].text_content()  
-    rank = el.cssselect("div.inner div.review-content div.rank")[0].text_content()
-    reviewtext = el.cssselect("div.inner div.review-content p")[0].text_content()  
-    #link = el.attrib['href']
-    #isbn = link.split("/")[2]
+      print title
+      print artist
+      #print link
+      #link = "http://www.readings.com.au" + link
+      record = {
+      "title" : title,
+      "artist" : artist,
+      "publisher" : publisher,
+      "rank" : rank,
+      "reviewtext" : reviewtext,
+      "scrape_date" : time.strftime( "%Y-%m-%d" )
     
-    print title
-    print artist
-    #print link
-    #link = "http://www.readings.com.au" + link
-    record = {
-    "title" : title,
-    "artist" : artist,
-    "publisher" : publisher,
-    "rank" : rank,
-    "reviewtext" : reviewtext,
-    "scrape_date" : time.strftime( "%Y-%m-%d" )
+      }
     
-    }
-    
-    scraperwiki.sqlite.save(unique_keys=["scrape_date", "rank"], data=record)
+      scraperwiki.sqlite.save(unique_keys=["scrape_date", "rank"], data=record)
+
+  page += 1
+  
+def scrapePage(url):
+  
+  html = None
+  attempts = 0
+  
+    while html == None and attempts < 3:
+      try: html = scraperwiki.scrape(url)
+      except:
+        attempts += 1
+        continue
+  
+      if html == None and attempts == 3:
+        print 'Unable to scrape ' + review_href
+
+    return html
